4cd379a6bd18421e3d44712aca7c668847f575bd
floushee <dominik.deutsch@gmail.com>
2020-10-29 13:42:27 +0100

    removed kuberadm join output from worker ansible playbook

--- k8s/join-and-configure.sh
+++ k8s/join-and-configure.sh
@@ -7,4 +7,4 @@ ssh -i ../ssh/packer -o "StrictHostKeyChecking=no" floushee@worker-01 sudo $join
 if [ -f ~/.kube/config ];then
   cp -f ~/.kube/config ~/.kube/config.`date '+%Y%m%d%H%M%s'`.bkp
 fi
-scp -i ../ssh/packer floushee@master-01:/home/floushee/.kube/config ~/.kube/config
+scp -i ../ssh/packer floushee@master-01:/home/floushee/.kube/config ~/.kube/k8s-lab.config
