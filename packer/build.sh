7e7c2f8b06f883a0a1dd0635ed924d6a0452601b
floushee <dominik.deutsch@gmail.com>
2020-10-29 08:57:31 +0100

    moved worker nodes back to 1gb ram and 1 cpu

--- packer/build.sh
+++ packer/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf output
 packer build -var 'vm_name=master-01' -var 'vm_static_ip=192.168.56.111' templates/centos-node.json 
-packer build -var 'vm_name=worker-01' -var 'vm_static_ip=192.168.56.112' templates/centos-node.json 
+packer build -var 'vm_name=worker-01' -var 'vm_static_ip=192.168.56.112' -var 'cpus=1' -var 'memory=1024' templates/centos-node.json 
