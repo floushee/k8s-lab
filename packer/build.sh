050b15c6eb6df6f322f1a1a2b64c26aac4f4bb08
floushee <dominik.deutsch@gmail.com>
2020-10-29 08:16:20 +0100

    2gb ram 2cpus for all nodes

--- packer/build.sh
+++ packer/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf output
-packer build -var 'vm_name=master-01' -var 'vm_static_ip=192.168.56.111' -var 'memory=2048' -var 'cpus=2' templates/centos-node.json 
+packer build -var 'vm_name=master-01' -var 'vm_static_ip=192.168.56.111' templates/centos-node.json 
 packer build -var 'vm_name=worker-01' -var 'vm_static_ip=192.168.56.112' templates/centos-node.json 
